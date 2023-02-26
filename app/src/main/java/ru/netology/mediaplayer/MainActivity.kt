e7e49f4af315905e964faf872825a8084463d7de
Vladimir Chistiakov <stomalux@yandex.ru>
2023-02-26 10:41:34 +1000

    лекция закончена

--- app/src/main/java/ru/netology/mediaplayer/MainActivity.kt
+++ app/src/main/java/ru/netology/mediaplayer/MainActivity.kt
@@ -6,13 +6,23 @@ import android.os.Bundle
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    private val observer = MediaLifecycleObserver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        lifecycle.addObserver(observer)
+
+
         findViewById<View>(R.id.play).setOnClickListener {
 
-            MediaPlayer.create(this,R.raw.a11).start()
+           observer.apply {
+                   // resources.openRawResourceFd(R.raw.a1).use { afd ->
+                   //mediaPlayer?.setDataSource(afd.fileDescriptor,afd.startOffset,afd.length)
+                   mediaPlayer?.setDataSource("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
+           }.play()
 
         }
     }
